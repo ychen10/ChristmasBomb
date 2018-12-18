763d13c7ab4e531868cf59784381b40f4a9ab10e
Chloe <ymoon@wellesley.edu>
2018-12-17 23:17:03 -0500

    comments

--- ChristmasTree.java
+++ ChristmasTree.java
@@ -13,33 +13,59 @@ public class ChristmasTree
     // change this later accordingly
     final int width=10 ; final int height=10;
     
-    // constructor
+    /**
+     * Constructor of the ChristmasTree class
+     */
     public ChristmasTree()
     {
-        // ********* change the values later
+        // (0,0) by default
         this.x = 0;
         this.y = 0;
     }
     
-    //returns the location variable of the ChristmasTree object
+    /**
+     * Returns the x-coordinate of the tree object
+     * 
+     * @return x coordinate of the tree
+     */
     public int getX(){
         return this.x;
     }
     
+    /**
+     * Returns the y-coordinate of the tree object
+     * 
+     * @return y coordinate of the tree
+     */
     public int getY(){
         return this.y;
     }
-    //modifies the location variable of the ChristmasTree object 
-    // based on the user input
+
+    /**
+     * Modifies the location of the ChristmasTree object 
+     * based on the input
+     * 
+     * @param desired x, y coordinate
+     */
     public void setLocation(int x, int y){
         this.x = x;
         this.y = y;
     }
     
+    /**
+     * Returns the width of the ChristmasTree object
+     * 
+     * @return width of the ChristmasTree object
+     */
     public int getWidth(){
         return width;
     }
     
+    /**
+     * Returns the height of the ChristmasTree object
+     * 
+     * @return height of the ChristmasTree object
+     */
     public int getHeight(){
         return height;
     }
--- StartPanel.java
+++ StartPanel.java
@@ -1,4 +1,3 @@
-
 import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
@@ -10,6 +9,14 @@ import javax.swing.border.*;
 import javax.swing.Timer;
 import sun.audio.*;
 
+/**
+ * Start Panel is displayed when the user starts the game and
+ * plays the audio file. Displays the game panel as the user
+ * presses the start button
+ *
+ * @author Yiran Chen
+ * @version 12/08/2018
+ */
 public class StartPanel extends JPanel {
   private JButton start;
   private JPanel gamePanel;
@@ -18,10 +25,16 @@ public class StartPanel extends JPanel {
   private Timer timer, scoreLabel;
   private int timeLeft = 120;
 
+  /**
+   * Constructor of the StartPanel Class
+   *
+   * @param a Game object
+   */
   public StartPanel(Game game) {
     this.game = game;
     setLayout(new FlowLayout(FlowLayout.CENTER));
     try {
+      // dispaly background
       label = new JLabel(); // background of the label
       label.setLayout(new FlowLayout(FlowLayout.CENTER));
       BufferedImage background = ImageIO.read(new File("background.png"));
@@ -29,6 +42,7 @@ public class StartPanel extends JPanel {
 
       gamePanel = new GamePanel(this.game);
 
+      // add it to game panel
       label.add(gamePanel);
       gamePanel.setVisible(false);
 
@@ -36,22 +50,28 @@ public class StartPanel extends JPanel {
       System.out.println(e);
     }
 
-      // JButton
-      start = new JButton("START");
-      StartListener listener = new StartListener();
-      start.addActionListener(listener);
-      label.add(start);
+    // Create the start button displayed on the start screen
+    start = new JButton("START");
+    // Create a Startlistener and add it to the button just created
+    StartListener listener = new StartListener();
+    start.addActionListener(listener);
+    label.add(start);
 
-      add(label);
-
+    // add label
+    add(label);
   }
 
+  /**
+   * Adds score and time to the panel
+   */
   public void addScoreTime() {
+    // create a JLabel "background" for score and time
     JLabel background = new JLabel();
     background.setLayout(new BoxLayout(background, BoxLayout.Y_AXIS));
     background.setPreferredSize(new Dimension(150, 400));
     //background.setOpaque(true);
 
+    // set the score label
     score = new JLabel("<html><center><h1>Score:<br>" + game.getScore() + "/ 1000</h1></center></html>");
     score.setBorder(new EmptyBorder(5,15,5,15));
     score.setOpaque(true);
@@ -59,32 +79,39 @@ public class StartPanel extends JPanel {
 
     background.add(Box.createRigidArea (new Dimension(0, 100)));
 
+    // time label
     time = new JLabel("<html><center><h1>Time Left:<br>" + timeLeft + "s</h1></center></html>", SwingConstants.CENTER);
     time.setBorder(new EmptyBorder(5,15,5,15));
     time.setBackground(Color.WHITE);
     time.setOpaque(true);
     background.add(time);
 
+    // add the labels to the background
     label.add(background);
   }
 
-  // takes care of score too
+  /**
+   * Uses the timer to start counting down the time (1000 sec)
+   * and changes the score and time accordingly
+   */
   public void startCountdown() {
+    // timer
     timer = new Timer(1000, new ActionListener() { // timer with 150 millisecond delay
       public void actionPerformed(ActionEvent e) {
+        // end the program if the game ends
         if (game.didEnd()) {
           timer.stop();
           return;
         }
+        // if time runs out, game ends
         if (timeLeft <= 0) {
           game.setLose();
           timer.stop();
           return;
         }
-        score.setText("<html><center><h1>  Score:<br>" + game.getScore() + " / 1000  </h1></center></html>");
-        //System.out.println("SCORE: "+game.getScore());
+        // decrements time
         timeLeft--;
-
+        // changes color of the letters when below 15 seconds
         if (timeLeft <= 15) {
           time.setForeground(Color.red);
           time.setText("<html><center><h1>Time Left:<br>" + timeLeft + "s</h1></center></html>");
@@ -93,16 +120,24 @@ public class StartPanel extends JPanel {
         }
       }
     });
-
+    
+    // score label
     scoreLabel = new Timer(20, new ActionListener() { // timer with 150 millisecond delay
       public void actionPerformed(ActionEvent e) {
         score.setText("<html><center><h1>Score:<br>" + game.getScore() + "/ 1000</h1></center></html>");
       }
     });
-
+    
+    // adding score label and timer
     scoreLabel.start();
     timer.start();
   }
+
+  /**
+   * StartListener class defines what happens when the start button is clicked.
+   * When the start button is clicked, the game displays the game panel and
+   * start the game accordingly by adding score, starting the timer, and playing the music.
+   */
   private class StartListener implements ActionListener {
     public void actionPerformed(ActionEvent event) {
       start.setEnabled(false);
@@ -116,7 +151,12 @@ public class StartPanel extends JPanel {
     }
   }
 
+
+  /**
+   * Plays music using AudioPlayer and the designated music file
+   */
   public static void music() {
+    // using AudioPlayer
     AudioPlayer MGP = AudioPlayer.player;
     AudioStream BGM;
     AudioData MD;
